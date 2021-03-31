bfb01eccfb8284ddbfb7066a7317b81db78bcb41
alioGRAF <49252993+alioGRAF@users.noreply.github.com>
2021-03-31 12:26:13 +0300

    Add files via upload

---
+++ ЛР3/lab3_1.js
@@ -0,0 +1,13 @@
+function sum(a) {
+    let currentSum = a;
+
+    function f(b) {
+        currentSum += b;
+        return f;
+    }
+    f.toString = function () {
+        return currentSum;
+
+    }
+    return f;
+}
